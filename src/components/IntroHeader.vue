72489d3768e88463be70540eaa733a78e9c6c36e
leetaewoo <taewoo5131@gmail.com>
2021-10-27 23:10:46 +0900

    first commit

---
+++ src/components/IntroHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="header">
+      <h3>{{introMsg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            introMsg: '코딩할때 노래듣기'
+        }
+    }
+}
+</script>
+
+<style>
+  #header{
+    background-color: goldenrod;
+    text-align: center;
+  }
+</style>
---
+++ src/views/Player.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      플레이어!
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Enter'
+}
+</script>
+
+<style>
+
+</style>
