0cf3abb0645cb77c309a854877ddbbdc37a39221
leetaewoo <taewoo5131@gmail.com>
2021-11-01 21:03:47 +0900

    music 파일 첨부 / audio태그 생성

--- src/components/musicSearch.vue
+++ src/components/musicSearch.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="introBtn">
-      <input type="text" placeholder="keyword" v-model="keyword">
-      <v-btn large v-on:click="search">{{introBtnMsg}}</v-btn>
-      <ul id="resultList">
-          <li v-for="music in resultMusic" v-bind:key="music.id">{{music.name}}</li>
-      </ul>
-      <audio autoplay></audio>
-      <router-view></router-view>
+        <!-- <input type="text" placeholder="keyword" v-model="keyword"> -->
+        <v-btn large v-on:click="play">{{introBtnMsg}}</v-btn>
+        <ul id="resultList">
+            <li v-for="music in resultMusic" v-bind:key="music.id">{{music.name}}</li>
+        </ul>
+        <audio autoplay controls>
+            <source src="@/music/01 - Epic Gardener - Anywhere.mp3" type="audio/mp3">
+        </audio>
+        <router-view></router-view>
   </div>
 </template>
 
@@ -15,7 +17,7 @@ export default {
     el: "#introBtn",
     data: function() {
         return {
-            introBtnMsg: 'search',
+            introBtnMsg: 'play',
             keyword: '',
             resultMusic: []
         }
@@ -45,6 +47,9 @@ export default {
                 alert('outer',e)
             })
         },
+        play: function() {
+            // <audio src="src/music/01 - Epic Gardener - Anywhere.mp3" autoplay></audio>
+        }
     }
 }
 </script>
